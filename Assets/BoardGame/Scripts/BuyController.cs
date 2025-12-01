33e8a67f4169645ce9d30eaeed49c503a4228ecf
WasTabon <72084063+WasTabon@users.noreply.github.com>
2025-12-01 22:16:19 +0200

    ui

--- Assets/BoardGame/Scripts/BuyController.cs
+++ Assets/BoardGame/Scripts/BuyController.cs
@@ -1,11 +1,10 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Purchasing;
-using UnityEngine.Purchasing.Extension;
 
-public class BuyController : MonoBehaviour, IDetailedStoreListener
+public class BuyController : MonoBehaviour
 {
-    public string _donateId = "com.htcpurchases.coinsmain";
+    public string _donateId = "com.upgarde.dualmode";
     
     public GameObject loadingButton;
     public AudioClip buySound;
@@ -14,50 +13,16 @@ public class BuyController : MonoBehaviour, IDetailedStoreListener
     public GameObject adPanel;
 
     private const string PurchaseKey = "AdPanelPurchased";
-    
-    private IStoreController _storeController;
-    private IExtensionProvider _extensionProvider;
 
     private void Start()
     {
         bool purchased = PlayerPrefs.GetInt(PurchaseKey, 0) == 1;
         adPanel.SetActive(!purchased);
-        
-        InitializePurchasing();
     }
 
-    private void InitializePurchasing()
+    public void OnPurchaseComplete(Product product)
     {
-        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
-        builder.AddProduct(_donateId, ProductType.NonConsumable);
-        UnityPurchasing.Initialize(this, builder);
-    }
-
-    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
-    {
-        _storeController = controller;
-        _extensionProvider = extensions;
-        
-        var product = _storeController.products.WithID(_donateId);
-        if (product != null && product.availableToPurchase)
-        {
-            OnProductFetched(product);
-        }
-    }
-
-    public void OnInitializeFailed(InitializationFailureReason error)
-    {
-        Debug.Log($"IAP Init Failed: {error}");
-    }
-
-    public void OnInitializeFailed(InitializationFailureReason error, string message)
-    {
-        Debug.Log($"IAP Init Failed: {error} - {message}");
-    }
-
-    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
-    {
-        if (args.purchasedProduct.definition.id == _donateId)
+        if (product.definition.id == _donateId)
         {
             Debug.Log("Complete");
             
@@ -68,17 +33,6 @@ public class BuyController : MonoBehaviour, IDetailedStoreListener
             PlayerPrefs.SetInt(PurchaseKey, 1);
             PlayerPrefs.Save();
             adPanel.SetActive(false);
-        }
-        
-        return PurchaseProcessingResult.Complete;
-    }
-
-    public void OnPurchaseFailed(Product product, PurchaseFailureReason reason)
-    {
-        if (product.definition.id == _donateId)
-        {
-            loadingButton.SetActive(false);
-            Debug.Log($"Failed: {reason}");
         }
     }
 
@@ -99,26 +53,18 @@ public class BuyController : MonoBehaviour, IDetailedStoreListener
 
     public void RestorePurchases()
     {
-        if (Application.platform == RuntimePlatform.IPhonePlayer ||
-            Application.platform == RuntimePlatform.OSXPlayer)
+        var storeListener = CodelessIAPStoreListener.Instance;
+        if (storeListener == null || !storeListener.HasProductInCatalog(_donateId))
         {
-            _extensionProvider.GetExtension<IAppleExtensions>().RestoreTransactions(OnRestoreComplete);
+            Debug.Log("Store not initialized");
+            return;
         }
-        else
-        {
-            Debug.Log("Android: покупки восстанавливаются автоматически");
-        }
+
+        storeListener.GetStoreExtensions<IAppleExtensions>()?.RestoreTransactions(OnRestoreComplete);
     }
 
     private void OnRestoreComplete(bool success, string error)
     {
-        if (success)
-        {
-            Debug.Log("Restore successful");
-        }
-        else
-        {
-            Debug.Log($"Restore failed: {error}");
-        }
+        Debug.Log(success ? "Restore successful" : $"Restore failed: {error}");
     }
 }
